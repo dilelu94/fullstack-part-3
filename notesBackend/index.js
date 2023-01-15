78de95da95cc6303fc18bcf2cb46193e744fbfb7
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-15 00:25:48 -0300

    3.6: Phonebook backend step6

--- notesBackend/index.js
+++ notesBackend/index.js
@@ -67,7 +67,7 @@ app.post('/api/notes', (request, response) => {
     const body = request.body
 
     if (!body.content) {
-        return response.status(400), json({
+        return response.status(400).json({
             error: 'content missing'
         })
     }
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -80,9 +80,15 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name) {
-        return response.status(400), json({
-            error: 'content missing'
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing or number missing'
+        })
+    }
+
+    if (persons.some(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 
