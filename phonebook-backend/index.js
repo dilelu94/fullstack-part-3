1ad54cdefe8ae56416cf13d2fc95cc760e0c1a03
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-18 02:30:42 -0300

    start of mongoose implementation

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,32 +3,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+/* mongoose */
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+const password = process.argv[2]
+const url = `mongodb+srv://fullstackluque:${password}@cluster0.q58hehj.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+/* end mongoose */
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
