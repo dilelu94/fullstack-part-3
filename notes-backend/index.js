fb4d1fbd5e71fad1030cc3a1b55d6d7e06bb8720
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-18 03:12:16 -0300

    app.post mongoose

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -11,36 +11,12 @@
 /* { name: 'John', age: 30 } object */
 /* {"name":"John","age":30} json, string */
 
-const { response, request } = require('express')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
 const Note = require('./models/note')
-/* mongoose start */
-const mongoose = require('mongoose')
-
-
-const url = `mongodb+srv://fullstackluque:${password}@cluster0.q58hehj.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-})
-
-noteSchema.set('toJSON', { //deletes __v and transform id from object to string
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-/* mongoose end */
 
 app.use(cors())
 app.use(express.json())
@@ -99,34 +75,33 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-/* Generating id */
+/* deprecated
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
         : 0
     return maxId + 1
-}
+} */
 
 /* Receiving data */
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note( {
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 /* home page */
@@ -148,7 +123,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
