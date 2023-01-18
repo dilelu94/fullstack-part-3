bbfbdaf943a1e9e4abcb21cd583db593be349677
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-17 23:32:17 -0300

    Exercise 3.12. 3.12: Command-line database

--- phonebook-backend/mongo.js
+++ phonebook-backend/mongo.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', true)
+
+/* node mongo.js yourpassword Name Number */
 
 if (process.argv.length < 3) {
     console.log('Please provide the password as an argument: node mongo.js <password>')
@@ -7,15 +10,33 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstackluque:${password}@cluster0.q58hehj.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstackluque:${password}@cluster0.q58hehj.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
 
 mongoose
     .connect(url)
     .then((result) => {
-        notes.find({}).then(result => {
-            result.forEach(note => {
-                console.log(note)
-            })
-            mongoose.connection.close()
+        console.log('connected')
+
+        const person = new Person({
+            name: process.argv[3],
+            number: process.argv[4]
         })
+
+        return person.save()
     })
+    .then(() => {
+        Person.find({}).then(result => {
+            result.forEach(person => {
+                console.log(person)
+            })
+        console.log('person saved!')
+        return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))})
