c12058b790293b71867892a8e5840c6055f7d6d5
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-19 02:22:11 -0300

    error middlware and moving stuff around

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -11,16 +11,11 @@
 /* { name: 'John', age: 30 } object */
 /* {"name":"John","age":30} json, string */
 
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+require('dotenv').config()
 const Note = require('./models/note')
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -38,52 +33,10 @@ app.use(cors())
 
 app.use(express.static('build'))
 
-/* Mongoose Fetching a single resource */
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id, (err, note) => {
-        if (err) return response.status(404).json({
-            error: '404 no matching id or another error'
-        })
-        if (note) {
-            response.json(note)
-        } else {
-            response.status(404).json({
-                error: '404 no matching id'
-            })
-        }
-    });
+/* home page */
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
 })
-
-/* update (aka put)*/
-app.put('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const updatedNote = request.body
-
-    Note.findByIdAndUpdate(id, updatedNote, { new: true }, (err, note) => {
-        if (err) {
-            return response.status(404).json({
-                error: 'Note not found'
-            })
-        }
-        response.json(note)
-    })
-})
-
-/* Deleting resources */
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(n => n.id !== id)
-
-    response.status(204).end()
-})
-
-/* deprecated
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-} */
 
 /* Mongoose Receiving data */
 app.post('/api/notes', (request, response) => {
@@ -106,11 +59,6 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-/* home page */
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 /* Mongoose string page */
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
@@ -118,12 +66,71 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
+/* Deleting resources */
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(n => n.id !== id)
+
+    response.status(204).end()
+})
+
+/* Mongoose Fetching a single resource */
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)
+        })
+})
+
+/* update (aka put)*/
+app.put('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const updatedNote = request.body
+
+    Note.findByIdAndUpdate(id, updatedNote, { new: true }, (err, note) => {
+        if (err) {
+            return response.status(404).json({
+                error: 'Note not found'
+            })
+        }
+        response.json(note)
+    })
+})
+
+/* deprecated
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+} */
+
 /* middleware (catch request made to non-existent routes) */
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
+/* Express error handlers are middleware */
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
