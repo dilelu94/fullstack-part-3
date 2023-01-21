3961251d6b88a295b81bc90fbd0822442eeb4b9c
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-21 19:56:11 -0300

    4.a Structure of backend application,

--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,38 +1,22 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then((result) => {
-    console.log(`connected to MongoDB ${result}`)
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
   date: {
     type: Date,
+    required: true,
   },
   important: Boolean,
 })
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
     returnedObject.id = returnedObject._id.toString()
-    // eslint-disable-next-line no-param-reassign, no-underscore-dangle
     delete returnedObject._id
-    // eslint-disable-next-line no-param-reassign, no-underscore-dangle
     delete returnedObject.__v
   },
 })
