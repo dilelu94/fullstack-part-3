9923b1d3a50224dca53918365205881275c0275b
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-30 20:36:59 -0300

    test a specific note can be viewed

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -18,7 +18,7 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const { body } = request
 
   const note = new Note({
@@ -26,12 +26,12 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
     date: new Date(),
   })
-
-  note.save()
-    .then((savedNote) => {
-      response.status(201).json(savedNote)
-    })
-    .catch((error) => next(error))
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
--- notes-backend/test/note_api.test.js
+++ notes-backend/test/note_api.test.js
@@ -78,6 +78,21 @@ test('note without content is not added', async () => {
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
 })
 
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(processedNoteToView)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
