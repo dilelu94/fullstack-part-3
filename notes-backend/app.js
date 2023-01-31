6ff8a7a424ae763ab2195a528b94fcd695a88e38
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-30 21:19:37 -0300

    refactor .delete with express-async-errors library

--- notes-backend/app.js
+++ notes-backend/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/order */
-const express = require('express')
 const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
 
 const app = express()
 const cors = require('cors')
